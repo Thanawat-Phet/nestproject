37e1e2c19074525d91cda2a2aa725bbb5eb9818d
Thanawat-phet <6830613017@psu.ac.th>
2026-02-24 11:39:41 +0700

    update Table

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { CustomerModule } from './customer/customer.module';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Dialect } from 'sequelize';
-
+import { Customer } from './customer/entities/customer.entity';
 @Module({
   imports: [ProductModule, YourinfoModule, UtilityModule, GlobalHelperModule, CustomerModule, ConfigModule.forRoot(),
     SequelizeModule.forRoot({
@@ -19,7 +19,7 @@ import { Dialect } from 'sequelize';
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    models: [], //Table
+    models: [Customer], //Table
   }),
   ProductModule,
   UtilityModule,
--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,1 +1,12 @@
-export class Customer {}
+import {Column,Model,Table} from 'sequelize-typescript';
+
+@Table({
+    tableName: 'customers',
+    timestamps: false,   
+})
+export class Customer extends Model{
+    @Column({})
+    fullname:string;
+    @Column({})
+    isActive:boolean;
+}
